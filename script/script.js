08d3c64c0a24512dd80e57e7be9c04f136369439
RSPrasangi <rebeccaprasangi@yahoo.co.in>
2024-08-08 16:14:53 +0100

    Update Script.js

---
+++ script/script.js
@@ -0,0 +1,5 @@
+// Custom JavaScript (optional)
+document.addEventListener('DOMContentLoaded', () => {
+    // Example of JavaScript functionality (optional)
+    console.log("Portfolio website is ready.");
+});
